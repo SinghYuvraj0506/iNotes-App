c68c1793867cdd7e8f99d7c92d2de9e59acc8a33
SinghYuvraj0506 <singhyuvraj0506@gmail.com>
2023-02-26 17:08:37 +0530

    small changes for rendering

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors())
 
 app.use(express.json())   // it is a middleware used to take req.body in json form
 
+app.get("/",async (req,res)=>{
+    res.send("Hellp guys, welcome to inotes server")
+})
+
 // Available Routes
 app.use("/api/auth",require('./routes/auth'))
 app.use("/api/notes",require('./routes/notes'))
